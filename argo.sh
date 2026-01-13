9a06b9c910db2114cb92c76593f847cf15d18241
Kristof-ay <kris200036@gmailcom>
2026-01-13 20:24:03 +0100

    argo and senkins modification

--- argo.sh
+++ argo.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
-
-
 kubectl create namespace argocd
+
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl get namespace argocd
 
 kubectl get svc -n argocd
 
+sleep 10 
+
 kubectl port-forward -n argocd svc/argocd-server 8082:443
 
-sleep 2
+sleep 10
 
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath={'.data.password'} | base64 --decode &&  echo
+
+
+
+
+
 
